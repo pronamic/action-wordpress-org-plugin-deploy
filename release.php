8b52f8924b085edcccf8cfa2cb6e72b8944863ca
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-08-02 14:00:30 +0200

    Update release.php

--- release.php
+++ release.php
@@ -1,5 +1,9 @@
 <?php
 
+echo '😏';
+echo getenv( 'GITHUB_REPOSITORY' );
+echo '🤔';
+
 $repository = getenv( 'REPOSITORY' );
 
 passthru( "gh release download --pattern 'pronamic-pay-with-rabo-smart-pay-for-woocommerce.1.0.0.zip' --repo $repository" );
