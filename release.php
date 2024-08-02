624a1295b845830048aaa8585f3add2b80b4140b
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-08-02 12:52:25 +0200

    wip

--- release.php
+++ release.php
@@ -1,3 +1,5 @@
 <?php
 
-passthru( "gh release download --pattern 'pronamic-pay-with-rabo-smart-pay-for-woocommerce.1.0.0.zip'" );
+$repository = getenv( 'REPOSITORY' );
+
+passthru( "gh release download --pattern 'pronamic-pay-with-rabo-smart-pay-for-woocommerce.1.0.0.zip' --repo $repository" );
